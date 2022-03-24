f35fc5d229e70361e0c74db509a5da14ec4ca8a1
Upkar <Upkar@DESKTOP-22L0J3T>
2022-03-24 23:39:13 +0530

    Add imageview

--- app/src/main/java/com/example/myapplication/MainActivity.java
+++ app/src/main/java/com/example/myapplication/MainActivity.java
@@ -5,7 +5,9 @@ import androidx.appcompat.app.AppCompatActivity;
 import android.os.Bundle;
 import android.util.Log;
 import android.view.View;
+import android.widget.Button;
 import android.widget.EditText;
+import android.widget.ImageView;
 import android.widget.Toast;
 
 public class MainActivity extends AppCompatActivity {
@@ -19,9 +21,20 @@ public class MainActivity extends AppCompatActivity {
         String msg="Hi "+username.getText().toString();
         Toast.makeText(this,msg,Toast.LENGTH_SHORT).show();
 }
+    ImageView imageView;
+    Button btn;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+
+        imageView=(findViewById(R.id.imageView));
+        btn=findViewById(R.id.loginButton);
+        btn.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                imageView.setImageResource(R.drawable.android);
+            }
+        });
     }
 }
